6db2cfb7fbb8a05df64a5ea98a2c1d1f12bf5b8b
MaksimkaSH <shteinmaks@gmail.com>
2024-03-01 13:30:56 +0300

    style: create interface PlacesService

--- src/main/kotlin/com/example/goodtripserver/places/route/PlacesController.kt
+++ src/main/kotlin/com/example/goodtripserver/places/route/PlacesController.kt
@@ -2,6 +2,7 @@ package com.example.goodtripserver.places.route
 
 import com.example.goodtripserver.places.model.PlaceRequest
 import com.example.goodtripserver.places.service.PlacesService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseBody
@@ -10,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class PlacesController {
 
+    @Autowired
+    private lateinit var placesService: PlacesService
     @GetMapping("/places")
     @ResponseBody
-    fun getNearPlaces(@RequestBody placeRequest: PlaceRequest): Any {//TODO make it suspend
-        return PlacesService().getNearPlaces(placeRequest)
+    fun getNearPlaces(@RequestBody placeRequest: PlaceRequest): Any {
+        return placesService.getNearPlaces(placeRequest)
     }
 }
