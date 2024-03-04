08d6784c2fc36f15edcb6c1e42ed509946a82345
Andrew Bolotsky <andrew04tlt@gmail.com>
2024-03-04 12:40:27 +0300

    refactor: add authentication repository implementation

---
+++ src/main/java/com/goodtrip/goodtripserver/database/repositories/AuthenticationRepositoryImplementation.java
@@ -0,0 +1,133 @@
+package com.goodtrip.goodtripserver.database.repositories;
+
+import com.goodtrip.goodtripserver.database.HibernateUtility;
+import com.goodtrip.goodtripserver.database.models.User;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+import java.util.Optional;
+
+
+/**
+ * Repository of authentication implementation.
+ */
+public class AuthenticationRepositoryImplementation implements AuthenticationRepository {
+
+
+    public Optional<String> getSalt(String username) {
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            TypedQuery<String> query = session.createQuery(
+                            "select m.salt from User m where m.username = :username", String.class)
+                    .setParameter("username", username);
+            List<String> results = query.getResultList();
+            if (results.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(results.getFirst());
+        }
+    }
+
+
+    public Optional<User> login(String username, String hashedPassword) {
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            TypedQuery<User> query = session.createQuery(
+                            "from User m where m.username = :username" +
+                                    " and m.hashedPassword = :hashedPassword", User.class)
+                    .setParameter("username", username)
+                    .setParameter("hashedPassword", hashedPassword);
+            List<User> results = query.getResultList();
+            if (results.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(results.getFirst());
+        }
+    }
+
+    public boolean signUpifNotExists(String username, String handle, String hashedPassword,
+                                     String hashedToken, String name, String surname, String salt) {
+        if (isUserExists(username) || !isTokenFree(hashedToken)) {
+            return false;
+        }
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.persist(new User(username, handle, hashedPassword, hashedToken, name, surname, salt));
+            transaction.commit();
+            return true;
+        }
+    }
+
+    /**
+     * Delete user.
+     *
+     * @param user User
+     */
+    private void deleteUser(User user) {
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            session.remove(user);
+            transaction.commit();
+        }
+    }
+
+
+    public void deleteUserIfExists(String username, String hashedPassword) {
+        Optional<User> user = login(username, hashedPassword);
+        if (user.isEmpty()) {
+            return;
+        }
+        deleteUser(user.get());
+    }
+
+
+    public boolean isUserExists(String username) {
+        return getSalt(username).isPresent();
+    }
+
+
+    public void updateToken(String username, String hashedPassword, String hashedToken) {
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            Query query = session.createQuery(
+                            "update User m set m.hashedToken = :hashedToken " +
+                                    " where m.username = :username" +
+                                    " and m.hashedPassword = :hashedPassword"
+                            , null).setParameter("hashedToken", hashedToken)
+                    .setParameter("username", username)
+                    .setParameter("hashedPassword", hashedPassword);
+            Transaction transaction = session.beginTransaction();
+            query.executeUpdate();
+            transaction.commit();
+        }
+    }
+
+
+    public Optional<User> loginUserWithToken(String hashedToken) {
+        try (Session session = HibernateUtility.getSessionFactory().openSession()) {
+            TypedQuery<User> query = session.createQuery(
+                            "from User m where m.hashedToken = :hashedToken", User.class)
+                    .setParameter("hashedToken", hashedToken);
+            List<User> results = query.getResultList();
+            if (results.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(results.getFirst());
+        }
+    }
+
+
+    public boolean isTokenFree(String hashedToken) {
+        return loginUserWithToken(hashedToken).isEmpty();
+    }
+
+
+    public void deleteUserWithToken(String hashedToken) {
+        Optional<User> user = loginUserWithToken(hashedToken);
+        if (user.isEmpty()) {
+            return;
+        }
+        deleteUser(user.get());
+    }
+
+}
