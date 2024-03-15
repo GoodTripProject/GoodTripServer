bfa8057b6cf25b0f80973bf238fcd878f0893bbc
Andrew Bolotsky <andrew04tlt@gmail.com>
2024-03-16 00:26:04 +0300

    refactor: refactor setters and getters of CityVisit

--- src/main/java/com/goodtrip/goodtripserver/database/models/CityVisit.java
+++ src/main/java/com/goodtrip/goodtripserver/database/models/CityVisit.java
@@ -1,32 +1,38 @@
 package com.goodtrip.goodtripserver.database.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.postgis.Point;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
 
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
-@Setter
-@Getter
+
 @Table(name = "city_visits", schema = "public", catalog = "GoodTripDatabase")
 public class CityVisit {
     private final static int SRID = 4326;
     @Id
+    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name="city")
+    @Setter
+    @Getter
+    @Column(name = "city")
     private String city;
 
-    @Column(name = "point", columnDefinition = "geometry(Point,"+SRID +")")
+
+    @Column(name = "point", columnDefinition = "geometry(Point," + SRID + ")")
     private Point point;
 
-    @Column(name="country_visit_id")
+    @Setter
+    @Getter
+    @Column(name = "country_visit_id")
     private Integer countryVisitId;
 
     public CityVisit(String city, double lon, double lat) {
