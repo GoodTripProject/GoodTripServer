2b1b1d19ca9aec1e3f7d082bbb4e05693157eb3e
Andrew Bolotsky <andrew04tlt@gmail.com>
2024-04-07 17:01:09 +0300

    feat: migrate AuthenticationRepository to jpa

--- src/main/java/com/goodtrip/goodtripserver/database/repositories/AuthenticationRepository.java
+++ src/main/java/com/goodtrip/goodtripserver/database/repositories/AuthenticationRepository.java
@@ -1,21 +1,18 @@
 package com.goodtrip.goodtripserver.database.repositories;
 
 import com.goodtrip.goodtripserver.database.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.util.Optional;
 
 /**
  * Repository of authentication.
  */
-public interface AuthenticationRepository extends CrudRepository<User,Integer> {
-    /**
-     * Get salt for user.
-     *
-     * @param username username (usually email)
-     * @return Optional.empty() if user does not exist, salt of user otherwise.
-     */
+@EnableTransactionManagement
+public interface AuthenticationRepository extends CrudRepository<User, Integer> {
+    @Query("SELECT salt from User where username = ?1")
     Optional<String> getSalt(String username);
 
     /**
@@ -26,19 +23,6 @@ public interface AuthenticationRepository extends CrudRepository<User,Integer> {
      * @return Optional.empty() if user have incorrect password or username, user otherwise
      */
     Optional<User> findUserByUsernameAndHashedPassword(String username, String hashedPassword);
-
-    /**
-     * Creates new user and add him to database (only if user with this login did not exist and token is unique).
-     *
-     * @param username       username (usually email)
-     * @param handle         handle of user
-     * @param hashedPassword hashed password
-     * @param name           name
-     * @param surname        surname
-     * @param salt           salt to save his password
-     * @return true if user added, false if user existed or token is not unique
-     */
-    boolean addUserIfNotExists(String username, String handle, String hashedPassword, String name, String surname, String salt);
 
     /**
      * Delete user from database if user exists.
@@ -54,5 +38,7 @@ public interface AuthenticationRepository extends CrudRepository<User,Integer> {
      * @param username username (usually email)
      * @return true if user exists in database, false otherwise
      */
-    boolean existsUserBy(String username);
+    boolean existsUserByUsername(String username);
+
+    Optional<User> getUserByUsername(String username);
 }
