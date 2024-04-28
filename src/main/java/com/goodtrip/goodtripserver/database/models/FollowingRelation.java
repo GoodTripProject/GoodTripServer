01b9461e68a0d001c492673b3d3c9fafa83f81e5
MaksimkaSH <shteinmaks@gmail.com>
2024-04-28 19:19:06 +0300

    feat: add following user to himself

--- src/main/java/com/goodtrip/goodtripserver/database/models/FollowingRelation.java
+++ src/main/java/com/goodtrip/goodtripserver/database/models/FollowingRelation.java
@@ -1,12 +1,16 @@
 package com.goodtrip.goodtripserver.database.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Builder
+@AllArgsConstructor
 @Table(name = "followers", schema = "public", catalog = "GoodTripDatabase")
 public class FollowingRelation {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -17,4 +21,9 @@ public class FollowingRelation {
     private Integer userId;
     @Column(name = "author_id")
     private Integer authorId;
+
+    public FollowingRelation(Integer userId, Integer authorId) {
+        this.userId = userId;
+        this.authorId = authorId;
+    }
 }
