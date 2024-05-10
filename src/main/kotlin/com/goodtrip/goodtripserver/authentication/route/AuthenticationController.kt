03bb9a50aa0dc149b9d35b696dbe026dfe7305be
MaksimkaSH <shteinmaks@gmail.com>
2024-05-10 18:11:06 +0300

    style: delete trash comments

--- src/main/kotlin/com/goodtrip/goodtripserver/authentication/route/AuthenticationController.kt
+++ src/main/kotlin/com/goodtrip/goodtripserver/authentication/route/AuthenticationController.kt
@@ -4,7 +4,6 @@ import com.goodtrip.goodtripserver.authentication.model.AuthenticationResponse
 import com.goodtrip.goodtripserver.authentication.model.AuthorizationRequest
 import com.goodtrip.goodtripserver.authentication.model.RegisterRequest
 import com.goodtrip.goodtripserver.authentication.service.AuthenticationService
-import kotlinx.coroutines.delay
 import lombok.RequiredArgsConstructor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -16,26 +15,22 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 class AuthenticationController {
-    private val LOG: Logger = LoggerFactory.getLogger(AuthenticationController::class.java)
+    private val logger: Logger = LoggerFactory.getLogger(AuthenticationController::class.java)
 
     @Autowired
     private lateinit var authenticationService: AuthenticationService
-//    var cnt = 0
 
     @ResponseBody
     @PostMapping("/login")
     suspend fun authorize(@RequestBody authorizationRequest: AuthorizationRequest): ResponseEntity<AuthenticationResponse> {
-//        if (cnt++ == 0) {
-//            delay(10000)
-//        }
-        LOG.info("authorizationRequest username:" + authorizationRequest.username + ", password:" + authorizationRequest.password)
+        logger.info("authorizationRequest username:" + authorizationRequest.username + ", password:" + authorizationRequest.password)
         return ResponseEntity.ok(authenticationService.login(authorizationRequest))
     }
 
     @ResponseBody
     @PostMapping("/register")
     suspend fun register(@RequestBody registerRequest: RegisterRequest): ResponseEntity<AuthenticationResponse> {
-        LOG.info("registerRequest username:" + registerRequest.username + ", password:" + registerRequest.password)
+        logger.info("registerRequest username:" + registerRequest.username + ", password:" + registerRequest.password)
         return ResponseEntity.ok(authenticationService.register(registerRequest))
     }
 
