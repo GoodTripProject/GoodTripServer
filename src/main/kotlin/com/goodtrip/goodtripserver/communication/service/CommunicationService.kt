f9f9914b1ee11dadc1db6ee99261404c0ce558dd
MaksimkaSH <shteinmaks@gmail.com>
2024-05-10 17:47:23 +0300

    feat: add CommunicationService

---
+++ src/main/kotlin/com/goodtrip/goodtripserver/communication/service/CommunicationService.kt
@@ -0,0 +1,16 @@
+package com.goodtrip.goodtripserver.communication.service
+
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service
+interface CommunicationService {
+    fun follow(userId: Int, authorId: Int): ResponseEntity<String>
+
+    fun unfollow(userId: Int, authorId: Int): ResponseEntity<String>
+
+    fun like(userId: Int, tripId: Int): ResponseEntity<String>
+
+    fun deleteLike(userId: Int, tripId: Int): ResponseEntity<String>
+
+}
