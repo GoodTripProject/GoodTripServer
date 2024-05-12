5bbb183c02b59a1a351bb397d6038dc10c6f0bd9
MaksimkaSH <shteinmaks@gmail.com>
2024-05-13 00:36:07 +0300

    feat: implement api for getting flight prices

---
+++ src/main/kotlin/com/goodtrip/goodtripserver/flights/route/FlightsController.kt
@@ -0,0 +1,25 @@
+package com.goodtrip.goodtripserver.flights.route
+
+import com.goodtrip.goodtripserver.flights.service.FlightsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/flights")
+class FlightsController {
+
+    @Autowired
+    private lateinit var flightsService: FlightsService
+
+    @GetMapping
+    fun getFlights(
+        @RequestParam(required = true) origin: String,
+        @RequestParam(required = true) destination: String,
+        @RequestParam(required = true) departureDate: String,
+        @RequestParam(required = true) adults: String,
+        @RequestParam(required = false) returnDate: String?
+    ) = flightsService.getFlights(origin, destination, departureDate, adults, returnDate)
+}
