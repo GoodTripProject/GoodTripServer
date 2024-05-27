3ce6ca758fed35732970990341980235a24a8065
Болоцкий Андрей Сергеевич <asbolotskiy@edu.hse.ru>
2024-05-27 16:41:45 +0300

    feat: update FollowersRepository for subscriptions

--- src/main/java/com/goodtrip/goodtripserver/database/repositories/FollowersRepository.java
+++ src/main/java/com/goodtrip/goodtripserver/database/repositories/FollowersRepository.java
@@ -1,9 +1,26 @@
 package com.goodtrip.goodtripserver.database.repositories;
 
 import com.goodtrip.goodtripserver.database.models.FollowingRelation;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 public interface FollowersRepository extends CrudRepository<FollowingRelation, Integer> {
+    @Modifying
+    @Query(value = "INSERT INTO followers (user_id, author_id) " +
+            "SELECT :userId,(SELECT id FROM users WHERE handle = :authorHandle);",
+            nativeQuery = true)
+    void followUsersByHandles(Integer userId, String authorHandle);
+
+    @Query("SELECT CASE WHEN EXISTS (SELECT 1 FROM FollowingRelation WHERE userId = :userId " +
+            "AND authorId = (SELECT id FROM User WHERE handle = :authorHandle)) THEN 1 ELSE 0 END")
+    boolean existsSubscription(Integer userId, String authorHandle);
+
+    @Modifying
+    @Query("DELETE FROM FollowingRelation WHERE userId = :userId " +
+            "AND authorId = (SELECT id FROM User WHERE handle = :authorHandle)")
+    void deleteSubscription(Integer userId, String authorHandle);
+
 }
