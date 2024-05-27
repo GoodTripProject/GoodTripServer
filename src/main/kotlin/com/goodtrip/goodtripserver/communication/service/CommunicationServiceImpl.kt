e5315be703409b140334e475533687ff03a92d46
MaksimkaSH <shteinmaks@gmail.com>
2024-05-27 21:28:45 +0300

    feat: call db functions

--- src/main/kotlin/com/goodtrip/goodtripserver/communication/service/CommunicationServiceImpl.kt
+++ src/main/kotlin/com/goodtrip/goodtripserver/communication/service/CommunicationServiceImpl.kt
@@ -1,34 +1,52 @@
 package com.goodtrip.goodtripserver.communication.service
 
+import com.goodtrip.goodtripserver.database.models.LikeRelation
+import com.goodtrip.goodtripserver.database.repositories.FollowersRepository
+import com.goodtrip.goodtripserver.database.repositories.LikeRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 
 @Service
 class CommunicationServiceImpl : CommunicationService {
+
+    @Autowired
+    private lateinit var followersRepository: FollowersRepository
+
+    @Autowired
+    private lateinit var likeRepository: LikeRepository
+
+
     override fun follow(userId: Int, author: String): ResponseEntity<String> {
-        if (false /* пользователь уже подписан*/) {
+        if (followersRepository.existsSubscription(userId, author)) {
             return ResponseEntity.badRequest().body("User is already subscribed")
         }
-        TODO("call db function")
+        followersRepository.followUserByHandles(userId, author)
         return ResponseEntity.ok().body("You are now subscribed to the user: $author")
     }
 
     override fun unfollow(userId: Int, author: String): ResponseEntity<String> {
-        if (false /* пользователь не подписан*/) {
+        if (!followersRepository.existsSubscription(userId, author)) {
             return ResponseEntity.badRequest().body("User is already unsubscribed")
         }
-        TODO("call db function")
+        followersRepository.deleteSubscription(userId, author)
         return ResponseEntity.ok().body("You are no longer following the user: $author")
     }
 
     override fun like(userId: Int, tripId: Int): ResponseEntity<String> {
-        TODO("call db function")
+        if (likeRepository.existsByUserIdAndTripId(userId, tripId)) {
+            return ResponseEntity.badRequest().body("Trip is already liked")
+        }
+        likeRepository.save(LikeRelation(tripId, userId))
         return ResponseEntity(HttpStatus.OK)
     }
 
     override fun deleteLike(userId: Int, tripId: Int): ResponseEntity<String> {
-        TODO("call db function")
+        if (!likeRepository.existsByUserIdAndTripId(userId, tripId)) {
+            return ResponseEntity.badRequest().body("Trip was not liked")
+        }
+        likeRepository.deleteByUserIdAndTripId(userId, tripId)
         return ResponseEntity(HttpStatus.OK)
     }
 }
