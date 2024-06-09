84085498a3d85159c82c8e8d67d08f3362f7784e
MaksimkaSH <shteinmaks@gmail.com>
2024-06-10 00:01:23 +0300

    style: remove extra parentheses

--- src/main/kotlin/com/goodtrip/goodtripserver/communication/route/CommunicationController.kt
+++ src/main/kotlin/com/goodtrip/goodtripserver/communication/route/CommunicationController.kt
@@ -4,7 +4,7 @@ import com.goodtrip.goodtripserver.communication.service.CommunicationService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
-@RestController()
+@RestController
 @RequestMapping("/communication")
 class CommunicationController {
 
