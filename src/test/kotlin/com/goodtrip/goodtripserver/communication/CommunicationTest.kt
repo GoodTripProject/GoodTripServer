42f9eb04cc26aca796e0e1c51ed10aed7d71cdd4
MaksimkaSH <shteinmaks@gmail.com>
2024-06-10 00:27:24 +0300

    test: add test for getting followers and subscriptions

--- src/test/kotlin/com/goodtrip/goodtripserver/communication/CommunicationTest.kt
+++ src/test/kotlin/com/goodtrip/goodtripserver/communication/CommunicationTest.kt
@@ -1,5 +1,6 @@
 package com.goodtrip.goodtripserver.communication
 
+import com.goodtrip.goodtripserver.database.models.User
 import com.goodtrip.goodtripserver.trip.utils.Utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -18,6 +19,30 @@ class CommunicationTest {
     fun followTest(){
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
+        val (token, handle, id) = Utils.`get token, handle and id`(headers, restTemplate)
+        val (_, handle2, id2) = Utils.`get token, handle and id`(headers, restTemplate)
+        headers.setBearerAuth(token)
+        val httpEntity = HttpEntity(null, headers)
+        val response = restTemplate.exchange(
+            "/communication/follow?userId=$id&author=$handle2",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        val response2 = restTemplate.exchange(
+            "/communication/follow?userId=$id2&author=$handle",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun doubleFollowingTest(){
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
         val (token, _, id) = Utils.`get token, handle and id`(headers, restTemplate)
         val (_, handle2, _) = Utils.`get token, handle and id`(headers, restTemplate)
         headers.setBearerAuth(token)
@@ -29,5 +54,140 @@ class CommunicationTest {
             String::class.java
         )
         assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        val response2 = restTemplate.exchange(
+            "/communication/follow?userId=$id&author=$handle2",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
     }
+
+    @Test
+    fun unfollowTest(){
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val (token, _, id) = Utils.`get token, handle and id`(headers, restTemplate)
+        val (_, handle2, _) = Utils.`get token, handle and id`(headers, restTemplate)
+        headers.setBearerAuth(token)
+        val httpEntity = HttpEntity(null, headers)
+        val response = restTemplate.exchange(
+            "/communication/follow?userId=$id&author=$handle2",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response.statusCode).isEqualTo(HttpStatus.OK)
+        val response2 = restTemplate.exchange(
+            "/communication/unfollow?userId=$id&author=$handle2",
+            HttpMethod.DELETE,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun strangeUnfollowTest(){
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val (token, handle, id) = Utils.`get token, handle and id`(headers, restTemplate)
+        val (_, handle2, id2) = Utils.`get token, handle and id`(headers, restTemplate)
+        headers.setBearerAuth(token)
+        val httpEntity = HttpEntity(null, headers)
+        val response1 = restTemplate.exchange(
+            "/communication/unfollow?userId=$id&author=$handle2",
+            HttpMethod.DELETE,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response1.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+
+        val response2 = restTemplate.exchange(
+            "/communication/unfollow?userId=$id2&author=$handle",
+            HttpMethod.DELETE,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.BAD_REQUEST)
+    }
+
+    @Test
+    fun getFollowersTest(){
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val (token, _, id) = Utils.`get token, handle and id`(headers, restTemplate)
+        val (_, handle2, id2) = Utils.`get token, handle and id`(headers, restTemplate)
+        headers.setBearerAuth(token)
+        val httpEntity = HttpEntity(null, headers)
+
+        val response1 = restTemplate.exchange(
+            "/communication/followers?userId=$id2",
+            HttpMethod.GET,
+            httpEntity,
+            Array<User>::class.java
+        )
+        assertThat(response1.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response1.body?.size).isEqualTo(1)
+
+        val response2 = restTemplate.exchange(
+            "/communication/follow?userId=$id&author=$handle2",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.OK)
+
+        val response3 = restTemplate.exchange(
+            "/communication/followers?userId=$id2",
+            HttpMethod.GET,
+            httpEntity,
+            Array<User>::class.java
+        )
+        assertThat(response3.statusCode).isEqualTo(HttpStatus.OK)
+        val body = response3.body
+        assertThat(body?.size).isEqualTo(2)
+        assertThat(body?.first()?.id).isEqualTo(id)
+    }
+
+
+    @Test
+    fun getSubscriptionsTest(){
+        val headers = HttpHeaders()
+        headers.contentType = MediaType.APPLICATION_JSON
+        val (token, _, id) = Utils.`get token, handle and id`(headers, restTemplate)
+        val (_, handle2, id2) = Utils.`get token, handle and id`(headers, restTemplate)
+        headers.setBearerAuth(token)
+        val httpEntity = HttpEntity(null, headers)
+
+        val response1 = restTemplate.exchange(
+            "/communication/subscriptions?userId=$id",
+            HttpMethod.GET,
+            httpEntity,
+            Array<User>::class.java
+        )
+        assertThat(response1.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(response1.body?.size).isEqualTo(1)
+
+        val response2 = restTemplate.exchange(
+            "/communication/follow?userId=$id&author=$handle2",
+            HttpMethod.POST,
+            httpEntity,
+            String::class.java
+        )
+        assertThat(response2.statusCode).isEqualTo(HttpStatus.OK)
+
+        val response3 = restTemplate.exchange(
+            "/communication/subscriptions?userId=$id",
+            HttpMethod.GET,
+            httpEntity,
+            Array<User>::class.java
+        )
+        assertThat(response3.statusCode).isEqualTo(HttpStatus.OK)
+        val body = response3.body
+        assertThat(body?.size).isEqualTo(2)
+        assertThat(body?.get(1)?.id).isEqualTo(id2)
+    }
+
+
 }
